95edd6d26512e611b6da589f54d874fe65a36996
this-is-batman <abhirupgupta234@rediffmail.com>
2023-08-15 00:13:59 +0530

    Added a test for the class containing the Static method.

--- tests/test_important.py
+++ tests/test_important.py
@@ -3,6 +3,13 @@ test_important.py: Script to test the pytest fixture which has been introduced i
 Author: Abhirup Gupta
 '''
 
+import sys
+import os.path
+path = os.path.abspath(__file__)
+source_code_location = os.path.dirname(os.path.dirname(path))
+sys.path.append(source_code_location + "/src/")
+from static_methods import Task
+
 def get_sum(a):
     return sum(a)
 
@@ -11,3 +18,9 @@ def test_if_important(important_value):
 
 def test_get_sum(int_list):
     assert get_sum(int_list) == 10
+
+# here, we are adding the test for the Static method
+def test_static_methods():
+    assert Task.get_cube(3) == 27
+    assert Task.get_square(2) == 4
+
